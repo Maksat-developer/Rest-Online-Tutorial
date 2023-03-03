a79e8f47b3717e6247ecd6b8e82ee39436b62074
Maksat-developer <gangsta.okulu02@gmail.com>
2023-03-03 21:04:43 +0600

    feature

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!j@4e(yki4jmz5^+gc-0*m4u$ry0g70%l=n*#%y@-_@sk^-u2-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # PACKAGES:
+    'rest_framework',
+
+
+
+    # APPS:
+    'product',
 ]
 
 MIDDLEWARE = [
---
+++ py_client/basic.py
@@ -0,0 +1,13 @@
+import requests
+
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint, json={"product_id": 123}) # HTTP Request
+print(get_response.headers)
+print(get_response.text)
+
+
+# print(get_response.text) # print raw text response
+# print(get_response.json())
+
+ 
