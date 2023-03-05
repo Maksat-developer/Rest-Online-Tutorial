2a124a45ce55e0f21ec7ecfdd67515ff45038162
Maksat-developer <gangsta.okulus02gmail.com>
2023-03-05 09:40:43 -0500

    create/app-api/add APIView

---
+++ backend/api/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from ..product.serializers import ProductSerializer
+
+
+
+@api_view(["POST", "GET"])
+def api_home(request, *args, **kwargs):
+    """
+        DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"Invalid":"Not good data"},status=status.HTTP_400_BAD_REQUEST)
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data = model_to_dict(model_data, fields=['id', 'title'])
+#     return HttpResponse(data)
+
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data['id'] = model_data.id
+#         data['title'] = model_data.title
+#         data['content'] = model_data.content
+#         data['price'] = model_data.price
+#         # model instance (model_data)
+#         # turn a Python dict
+#         # retutrn JSON to my client
+
+#     return JsonResponse(data)
+
